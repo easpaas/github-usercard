c5d65896cf16119c40ff55068440205970fa4329
evan <aspaasevan@gmail.com>
2020-03-12 10:24:05 -0700

    card displays correct data

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,15 +1,15 @@
 /* Step 1: using axios, send a GET request to the following URL 
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
-*/
-axios.get('https://api.github.com/users/easpaas')
+*/  
+axios.get(`https://api.github.com/users/easpaas`)
   .then(response => {
-    console.log(response.data.bio);
+    console.log(response.data);
+    cards.append(createCard(response.data));
   })
   .catch(error => {
     console.log(error);
-  })
-
+  });
 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
@@ -35,7 +35,7 @@ axios.get('https://api.github.com/users/easpaas')
 const followersArray = [];
 
 /* Step 3: Create a function that accepts a single object as its only argument,
-          Using DOM methods and properties, create a component that will return the following DOM element:
+           Using DOM methods and properties, create a component that will return the following DOM element:
 
 <div class="card">
   <img src={image url of user} />
@@ -53,40 +53,65 @@ const followersArray = [];
 </div>
 
 */
-function create(obj) {
+
+/*
+ * This function will accept a data object 
+ * that will be response.data from axios get.
+ */ 
+function createCard(data) {
+  // card ('div')
   const card = document.createElement('div');
   card.classList.add('card');
 
   const image = document.createElement('img');
+  image.setAttribute('src', data.avatar_url);
+  card.append(image);
+
+  // cardInfo ('div')
   const cardInfo = document.createElement('div');
   cardInfo.classList.add('card-info');
 
-  card.append(img);
-  // might need to move this down more
-  card.append(cardInfo);
-
   // ********************************
   // append all of these to card-info
+  
+  // name of user ('h3')
   const user = document.createElement('h3');
   user.classList.add('name');
+  user.textContent = data.name
 
+  // username ('p')
   const username = document.createElement('p');
   username.classList.add('p');
+  username.textContent = data.login;
 
+  // location ('p')
   const location = document.createElement('p');
-  const profile = document.createElement('p');
-  const pLink = document.createElement('a');
+  location.textContent = data.location;
 
-  profile.append(pLink);
+  const profile = document.createElement('p');
   
+  // pLink ('a')
+  const pLink = document.createElement('a');
+  pLink.setAttribute('src', data.html_url);
+  profile.append(pLink);
+
+  // followers count ('p')
   const followers = document.createElement('p');
+  followers.textContent = data.followers;
+
+  // following count ('p')
   const following = document.createElement('p');
+  following.textContent = data.following;
+
+  // bio ('p')
   const bio = document.createElement('p');
+  bio.textContent = data.bio;
 
   cardInfo.append(user, username, location, profile, followers, following, bio);
-  
   // ********************************
   
+  card.append(cardInfo);
+
   return card;
 } // closes create function
 
@@ -97,3 +122,6 @@ function create(obj) {
   luishrd
   bigknell
 */
+
+const cards = document.querySelector('.cards');
+// cards.append(createCard('easpaas'));
